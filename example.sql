e4e1c6186337e1a0cc0ba02d6700ad39bc2d60b4
chenhaohaochenlulu <xhfch@126.com>
2017-04-04 00:26:05 +0800

    Add example.sql

---
+++ example.sql
@@ -0,0 +1,10 @@
+
+DELETE FROM `knowledge`.`k_links` WHERE `idbooklink`='1';
+DELETE FROM `knowledge`.`k_links` WHERE `idbooklink`='2';
+
+ALTER TABLE `knowledge`.`k_links` 
+AUTO_INCREMENT = 1 ;
+
+
+INSERT INTO `knowledge`.`k_links` (`who`, `what`, `what_type`, `where`, `status`, `remark`) 
+VALUES ('who', 'what', 'type', 'where', 'status', 'remark');
